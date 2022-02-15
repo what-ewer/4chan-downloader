0c6e37b61b59854bdd59066d00375a2c09fd06cd
whatewer <57753819+what-ewer@users.noreply.github.com>
2022-02-15 21:39:37 +0100

    Remove unnecessary print

--- src/parser.py
+++ src/parser.py
@@ -23,5 +23,4 @@ class SettingsParser:
                 'MinimalBoardRefreshTime'),
             min_replies=self.config['SETTINGS'].getint('MinimumReplies')
         )
-        print(vars(settings))
         return settings
